501a51a17f36c4e3d16d429e6f45cdc1ccd28063
kosti996 <kostiukevycha@gmail.com>
2025-02-11 17:22:54 +0100

    base bar chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChartComponent } from "./chart/chart.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [ChartComponent]
 })
 export class AppComponent {
   title = 'fincharts-app';
+
 }
---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables);
+
+const data = {
+  labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'July'],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+@Component({
+  selector: 'app-chart',
+  imports: [],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.scss'
+})
+export class ChartComponent implements OnInit{
+  chart: any;
+  config: any = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+
+  ngOnInit() {
+    this.chart = new Chart('MyChart', this.config);
+  }
+}
