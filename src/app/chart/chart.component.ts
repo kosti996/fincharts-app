1063f560fd762bcf9225548892ff205117f13351
kosti996 <kostiukevycha@gmail.com>
2025-02-11 19:23:17 +0100

    added chart-financial

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,33 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart, registerables } from 'chart.js'
 Chart.register(...registerables);
+import { CandlestickElement, CandlestickController } from 'chartjs-chart-financial';
+Chart.register(CandlestickElement, CandlestickController);
+import 'chartjs-adapter-moment';
 
-const data = {
-  labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'July'],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(255, 205, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(201, 203, 207, 0.2)'
-    ],
-    borderColor: [
-      'rgb(255, 99, 132)',
-      'rgb(255, 159, 64)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)',
-      'rgb(201, 203, 207)'
-    ],
-    borderWidth: 1
-  }]
-};
+const barData = [
+  {}
+];
 
 @Component({
   selector: 'app-chart',
@@ -35,21 +15,18 @@ const data = {
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.scss'
 })
-export class ChartComponent implements OnInit{
+export class ChartComponent implements OnInit {
   chart: any;
-  config: any = {
-    type: 'bar',
-    data: data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    },
-  };
 
   ngOnInit() {
-    this.chart = new Chart('MyChart', this.config);
+    this.chart = new Chart('MyChart', {
+      type: 'candlestick',
+      data: {
+        datasets: [{
+          label: 'CHRT - Chart.js Corporation',
+          data: barData,
+        }]
+      }
+    });
   }
 }
