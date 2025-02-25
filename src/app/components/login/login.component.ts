8b2840c24560d6a2da620e6210afecd1460e35f0
kosti996 <kostiukevycha@gmail.com>
2025-02-25 20:37:46 +0100

    added styles to login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -13,17 +16,17 @@ export class LoginComponent {
   username: string = 'r_test@fintatech.com';
   password: string = 'kisfiz-vUnvy9-sopnyv';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   onSubmit() {
     this.authService.login(this.username, this.password).subscribe({
       next: (response) => {
         this.authService.setToken(response.access_token);
+        this.router.navigate(['historical-chart']);
       },
       error: (err) => {
         console.error('get token failed', err);
       }
     });
-    this.router.navigate(['historical-chart']);
   }
 }
